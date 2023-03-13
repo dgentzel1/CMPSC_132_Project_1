f400185cf44c75ad36d4bd1f4c8c329f87bd1971
dgentzel1 <dgentzel1@gmail.com>
2023-03-12 23:29:20 -0400

    Completed Date class

---
+++ Date.py
@@ -0,0 +1,43 @@
+# Name: Christian Ankney and David Gentzel
+# Course: CMPSC 132
+# File Name: Date.py
+# Date: 3/17/23
+#
+# Short Description: Store information regarding the date
+
+class Date:
+
+    # Constructor
+    def __init__(self, day=00, month=00, year=0000):
+        self.__day = day
+        self.__month = month
+        self.__year = year
+
+    # Day mutator
+    def setDay(self, day):
+        self.__day = day
+
+    # Day accessor
+    def getDay(self):
+        return self.__day
+
+    # Month mutator
+    def setMonth(self, month):
+        self.__month = month
+
+    # Month accessor
+    def getMonth(self):
+        return self.__month
+
+    # Year mutator
+    def setYear(self, year):
+        self.__year = year
+
+    # Year accessor
+    def getYear(self):
+        return self.__year
+
+    # Print out date information
+    def __str__(self):
+        return f'\nThe date is: {self.__day}/{self.__month}/{self.__year}.'
+
