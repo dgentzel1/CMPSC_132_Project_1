9e5d53b61300193f0fca24a55a65d9045c358427
dgentzel1 <dgentzel1@gmail.com>
2023-03-13 13:45:57 -0400

    Completed Address class

---
+++ Address.py
@@ -0,0 +1,51 @@
+# Name: Christian Ankney and David Gentzel
+# Course: CMPSC 132
+# File Name: Address.py
+# Date: 3/17/23
+#
+# Short Description: Store address information
+
+class Address:
+
+    # Constructor
+    def __init__(self, street='Street', city='City', state='ST', zipcode='*****'):
+        self.__strNo = street
+        self.__city = city
+        self.__state = state
+        self.__zipcode = zipcode
+
+    # Street mutator
+    def setStreet(self, street):
+        self.__strNo = street
+
+    # Street accessor
+    def getStreet(self):
+        return self.__strNo
+
+    # City mutator
+    def setCity(self, city):
+        self.__city = city
+
+    # City accessor
+    def getCity(self):
+        return self.__city
+
+    # State mutator
+    def setState(self, state):
+        self.__state = state
+
+    # State accessor
+    def getState(self):
+        return self.__state
+
+    # Zipcode mutator
+    def setZipCode(self, zc):
+        self.__zipcode
+
+    # Zipcode accessor
+    def getZipCode(self):
+        return self.__zipcode
+
+    # String formatter
+    def __str__(self):
+        return f'{self.__strNo}, {self.__city}, {self.__state} {self.__zipcode}'
